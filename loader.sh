ae9e422dcd5bed5772f589845a5586b8a9e48534
eastpiger <lvwt@shanghaitech.edu.cn>
2016-09-07 21:38:09 +0800

    change loader

--- loader.sh
+++ loader.sh
@@ -5,7 +5,7 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
 
-nohup gunicorn shanghaitechSIST.wsgi:application -b 127.0.0.1:8080 --workers 4 --worker-connections 65535&
+nohup gunicorn shanghaitech_SIST.wsgi:application -b 127.0.0.1:8080 --workers 4 --worker-connections 65535&
 service nginx start
 
 while true
