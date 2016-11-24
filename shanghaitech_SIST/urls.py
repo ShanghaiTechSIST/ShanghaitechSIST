c5a718b00398a5cd05e640da1a38fc009720dbbf
eastpiger <lvwt@shanghaitech.edu.cn>
2016-11-24 13:20:39 +0800

    change url to enable admin;

--- shanghaitech_SIST/urls.py
+++ shanghaitech_SIST/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = i18n_patterns(
-    url(r'^yizhihongxingchuqianglai/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cms.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
